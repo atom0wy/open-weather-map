73862e04abe0edecf7eb95240b5dbd1dacb8d481
Adam Dyczkowski <atomdyczkowski@gmail.com>
2021-08-11 10:09:04 +0200

    generated spring project

---
+++ src/test/java/pl/atom/weather/OpenWeatherMap/OpenWeatherMapApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.atom.weather.OpenWeatherMap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenWeatherMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
